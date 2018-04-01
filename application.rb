fb4b6be944505da5d3547e02631f11ac90b60031
github username <jeremyagottfried@gmail.com>
2018-04-01 18:51:59 -0400

    Done.

--- application.rb
+++ application.rb
@@ -1,10 +1,11 @@
 class Application
 
+
   def call(env)
+    arr = ["hello, world", "my name is Jeremy", "this is my first local server run", "and it makes me very happy"]
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Jeremy"
     resp.finish
   end
 
 end
-
