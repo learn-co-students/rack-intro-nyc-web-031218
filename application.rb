80af25401eb741e18f7515b06b56f54dd74e4504
Helen Liutongco <liutongcoh@gmail.com>
2018-04-01 17:37:33 -0400

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,9 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    name = "Helen"
+    resp.write "Hello, my name is #{name}"
     resp.finish
   end
 
 end
-
